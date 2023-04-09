a89fb9b0c23e95428f69461436a242c852b7f44d
Ashiqur Rahman Biplop <biplops22@gmail.com>
2023-04-09 12:30:47 +0600

    added some components

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,36 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import About from "./components/About";
+import Home from "./components/Home";
+import ErrorPage from "./components/ErrorPage";
+import Shop from "./components/Shop";
 
 const router = createBrowserRouter([
-    {
-        path:'/',
-        element:<App></App>
-    }
-])
+  {
+    path: "/",
+    element: <App></App>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: 'shop',
+        element:<Shop></Shop>,
+        loader: () => fetch('Products.json')
+      },
+      {
+        path: "/about",
+        element: <About></About>,
+      },
+    ],
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(<RouterProvider router={router}></RouterProvider>)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router}></RouterProvider>
+);
